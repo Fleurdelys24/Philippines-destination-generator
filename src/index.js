45cdc208007296810175ac24c71f9b43bea822b9
Fleurdelys24 <chenglisa888@yahoo.com>
2025-02-14 18:59:18 +0800

    Added basic form handling and added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateDestination(event) {
+  event.preventDefault();
 
+  new Typewriter("#destination", {
+    strings: "Palawan",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let destinationFormElement = document.querySelector(
+  "#Philippines-destination-generator-form"
+);
+destinationFormElement.addEventListener("submit", generateDestination);
